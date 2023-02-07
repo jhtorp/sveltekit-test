127fb0c864243747d363f4af5b1db76410d8927a
jhtorp <jhtorp@hotmail.com>
2023-02-07 13:16:55 +0100

    fixed hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		appDir: 'app',
+		prerender: {
+			default: true,
+		},
+		trailingSlash: 'always',
 	}
 };
 
